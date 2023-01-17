7153741984a1453c3eb23e65566022f2ec9766da
SametYurtoglu <yrtlsamet@gmail.com>
2023-01-17 18:44:42 +0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
 <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300;800&display=swap" rel="stylesheet"/>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" async></script>
+<script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" async></script>
       <body className='bg-gray-900'>
         <Main />
         <NextScript />
